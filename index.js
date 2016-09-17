3ac2ec478d6080660809794c1071d7047deba8b4
Alexander Kolesov <kolesov.alex.s@gmail.com>
2016-09-17 13:04:15 +0300

    Simple chat not commited files

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
 let config = require('./utils/config');
 let logger = require('./utils/logger');
 let loggerMiddleware = require('./utils/loggerMiddleware');
 
 
-app = express();
-
 app.use(loggerMiddleware);
+app.use('/', express.static(__dirname + '/public'));
 
-app.route('/').get(function (req, res) {
-  res.send('Hello, World!');
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
 
-app.listen(config.get('express:port'), () => {
-  logger.info('Listening on port ' + config.get('express:port'));
+
+server.listen(config.get('server:port'), (err) => {
+  if (err) {
+    logger.info('Can not start server');
+  } else {
+    logger.info('Server started on port: ' + config.get('server:port'));
+  }
 });
